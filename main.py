7ba2bad0190336ec2ee36a3969e82edf63f41c8e
Ze-hs <thehao33@gmail.com>
2026-08-30 10:41:15 -0400

    Added name extraction and azure translation

--- main.py
+++ main.py
@@ -1,12 +1,53 @@
+import json
+import logging.config
 import os
+from pathlib import Path
+from typing import List
 
+import requests, uuid, json
 import spacy
 from dotenv import load_dotenv
 
 load_dotenv()
+logger = logging.getLogger("TextFlow")
 
-nlp = spacy.blank("en")
-nlp.add_pipe("sentencizer")
+nlp = spacy.load("en_core_web_sm")
+
+def translate_name(name: str, src_lang: str, target_langs: List[str]):
+    return azure_translate(name, src_lang, target_langs)[0]["translations"][0]["text"]
+
+def azure_translate(name: str, src_lang: str, target_langs: List[str]):
+
+    key = os.environ.get("AZURE_TRANSLATOR")
+    endpoint = "https://api.cognitive.microsofttranslator.com"
+
+    location = os.environ.get("AZURE_REGION")
+
+    path = '/translate'
+    constructed_url = endpoint + path
+
+    params = {
+        'api-version': '3.0',
+        'from': src_lang,
+        'to': target_langs
+    }
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': key,
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+                        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    # You can pass more than one object in body.
+    body = [{
+        'text': name,
+    }]
+
+    request = requests.post(constructed_url, params=params, headers=headers, json=body)
+    response = request.json()
+    print(json.dumps(response, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': ')))
+    return response
 
 def open_router_call(text: str):
     import requests
@@ -32,14 +73,13 @@ def open_router_call(text: str):
         })
     )
 
-    # Extract the assistant message with reasoning_details
     response = response.json()
     response = response['choices'][0]['message']
 
     return response.get('content')
 
 def load_file(path: str):
-    with open(path, "r") as f:
+    with open(path, "r", encoding="utf-8") as f:
         text = f.read()
     return text
 
@@ -67,18 +107,34 @@ def save(text: str, path: str):
     with open(path, "a+", encoding="utf-8") as f:
         f.write(text)
 
+def extract_names(text: str):
+    doc = nlp(text)
+    names = set([ent.text for ent in doc.ents if ent.label_ in {"PERSON", "ORG", "GPE", "LOC", "FAC"}])
+    logger.debug(f"Extracted Names: {'\n'.join(names)}\n")
+    return names
+
+def set_up_logger():
+    Path("debug/logs").mkdir(exist_ok=True, parents=True)
+
+    with open(Path("./logging.json")) as file:
+        config = json.load(file)
+
+    logging.config.dictConfig(config=config)
 
 def main():
-    with open("translate.txt", "w", encoding="utf-8") as f:
-        f.write("")
-
-    text = load_file("test.txt")
-
-    chunks = split_text(text)
-    for chunk in chunks:
-        translated_text = translate(chunk)
-        save(translated_text, "translate.txt")
-
+    azure_translate("小那海あや", "ja", ["en"])
+    # set_up_logger()
+    # with open("translate.txt", "w", encoding="utf-8") as f:
+    #     f.write("")
+    #
+    # text = load_file("test.txt")
+    #
+    # chunks = split_text(text)
+    # for index, chunk in enumerate(chunks):
+    #     logger.debug(f"Chunk {index}")
+    #     extract_names(chunk)
+    #     translated_text = translate(chunk)
+    #     save(translated_text, "translate.txt")
 
 if __name__ == "__main__":
     main()
